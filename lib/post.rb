2937c265c982dd4f59e8470bb9c70c1001a31872
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-06 23:27:18 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+  attr_accessor :author, :name
+   @@all = []
+ 
+ 
+  def initialize(name)
+    @name = name
+    save
+  end
+ 
+ def save
+    @@all << self
+ end  
+ 
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end  
+  
+ def self.all
+    @@all
+ end  
+  
+end
